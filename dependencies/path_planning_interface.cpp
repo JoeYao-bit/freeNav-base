763313e2d161e6e8b0d9ad731ef3b25a71b06151
yaozhuo <1521232476@qq.com>
2024-04-14 19:04:56 +0800

    update

--- dependencies/path_planning_interface.cpp
+++ dependencies/path_planning_interface.cpp
@@ -3,3 +3,30 @@
 //
 
 #include "path_planning_interface.h"
+#include <fstream>
+
+namespace freeNav {
+
+    void appendToFile(const std::string &file_name, const std::string &content, bool prune) {
+        // 3, save result to file
+        std::ofstream os(file_name, prune ? std::ios_base::out : std::ios_base::app);
+        //os << "TYPE START TARGET TIME_COST MAKE_SPAN TOTAL_LENGTH " << std::endl;
+        os << content << std::endl;
+        os.close();
+    }
+
+
+    void printCurrentTime() {
+        time_t curtime;
+        time(&curtime);
+//        std::cout << "1970到目前经过秒数:" << time(&curtime) << std::endl;
+        std::cout << "本地日期和时间:" << ctime(&curtime);
+//        tm *nowtime = localtime(&curtime);
+//        // 输出tm结构的年月日
+//        std::cout << "年: " << 1900 + nowtime->tm_year << std::endl;
+//        std::cout << "月: " << 1 + nowtime->tm_mon << std::endl;
+//        std::cout << "日: " << nowtime->tm_mday << std::endl;
+//        std::cout << "时间: " << nowtime->tm_hour << ":";
+//        std::cout << nowtime->tm_min << ":" << std::endl;
+    }
+}
--- dependencies/path_planning_interface.h
+++ dependencies/path_planning_interface.h
@@ -97,27 +97,30 @@ namespace freeNav {
     template <Dimension N>
     using MAPF_FUNC_TESTS = std::vector<MAPF_FUNC_TEST<N> >;
 
+    void appendToFile(const std::string& file_name, const std::string& content, bool prune=false);
+
+    void printCurrentTime();
+
     // a couple of start and target for multiple methods
     // return true is all pass, return false is one is failed
     template <Dimension N>
     bool MAPFPathPlanningsTest(DimensionLength* dim, const IS_OCCUPIED_FUNC<N> & isoc, const Instances<N> & ists,
                                      const MAPF_FUNC_TESTS<N>& mapf_path_plannings,
-                                     StatisticS& statistics, OutputStreamS& output_streams) {
-        statistics.clear();
-        output_streams.clear();
+                                     const std::string& file_path, bool append=false) {
         bool is_failed = false;
         for(int i=0; i<mapf_path_plannings.size(); i++) {
             const auto& path_planning = mapf_path_plannings[i];
             Statistic statistic;
             OutputStream output_stream;
             Paths<N> path;
+            //std::cout << " ists " << ists << std::endl;
             path_planning(dim, isoc, ists, path, statistic, output_stream);
-            statistics.push_back(statistic);
-            output_streams.push_back(output_stream);
+            appendToFile(file_path, output_stream, append);
             if(path.empty()) {
                 std::cout << "planning uses method " << i << " failed " << std::endl;
                 is_failed = true;
             }
+            printCurrentTime();
         }
         return !is_failed;
     }
@@ -127,23 +130,19 @@ namespace freeNav {
                                         const IS_OCCUPIED_FUNC<N> & isoc,
                                         const InstancesS<N> & ists,
                                         const MAPF_FUNC_TESTS<N>& mapf_path_plannings,
-                                        StatisticSS& statistics, OutputStreamSS& output_streams) {
-        statistics.clear();
-        output_streams.clear();
+                                        const std::string& file_path, bool append=false) {
         bool all_success = true;
         for(int i=0; i<ists.size(); i++) {
             const auto& ist = ists[i];
-            StatisticS statistic;
-            OutputStreamS output_stream;
-            bool success = MAPFPathPlanningsTest(dim, isoc, ist, mapf_path_plannings, statistic, output_stream);
+            bool success = MAPFPathPlanningsTest(dim, isoc, ist, mapf_path_plannings, file_path, append);
             std::cout << "-- finish " << i << " th instance " << std::endl;
             if(all_success) all_success = success;
-            statistics.push_back(statistic);
-            output_streams.push_back(output_stream);
         }
         std::cout << " finish " << ists.size() << " cases" << std::endl;
         return all_success;
     }
+
+    void printCurrentTime();
 
 }
 

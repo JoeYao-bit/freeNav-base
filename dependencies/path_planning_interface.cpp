4a4d9f8fb2be6523a07adec7f47993070ad935f1
remote_yz <152132476@qq.com>
2024-04-19 20:19:00 +0800

    update printCurrentTime

--- dependencies/path_planning_interface.cpp
+++ dependencies/path_planning_interface.cpp
@@ -17,14 +17,14 @@ namespace freeNav {
     void printCurrentTime() {
         time_t curtime;
         time(&curtime);
-//        std::cout << "1970到目前经过秒数:" << time(&curtime) << std::endl;
-        std::cout << "本地日期和时间:" << ctime(&curtime);
+//        std::cout << "seconds from 1970:" << time(&curtime) << std::endl;
+        std::cout << "current date and time:" << ctime(&curtime);
 //        tm *nowtime = localtime(&curtime);
-//        // 输出tm结构的年月日
-//        std::cout << "年: " << 1900 + nowtime->tm_year << std::endl;
-//        std::cout << "月: " << 1 + nowtime->tm_mon << std::endl;
-//        std::cout << "日: " << nowtime->tm_mday << std::endl;
-//        std::cout << "时间: " << nowtime->tm_hour << ":";
+//        // output structured year/month/day
+//        std::cout << "year: " << 1900 + nowtime->tm_year << std::endl;
+//        std::cout << "month: " << 1 + nowtime->tm_mon << std::endl;
+//        std::cout << "day: " << nowtime->tm_mday << std::endl;
+//        std::cout << "hour: " << nowtime->tm_hour << ":";
 //        std::cout << nowtime->tm_min << ":" << std::endl;
     }
 
